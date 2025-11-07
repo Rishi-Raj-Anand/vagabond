04283d5843d9f18dbe19a45b4917b0d19f88edd5
Rishi-Raj-Anand <rishirajanand309@gmail.com>
2025-11-07 17:33:57 +0530

    phase 1 complete

--- models/listing.js
+++ models/listing.js
@@ -9,7 +9,11 @@ const listingSchema = new mongoose.Schema({
     description: String,
     image: {
         type: String,
-        set: (v) => v === "" ? "https://images.unsplash.com/photo-1499793983690-e29da59ef1c2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2glMjBob3VzZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60" : v.url,
+        set:(v)=>(v==""|| v==null||v==undefined)?
+            "https://images.unsplash.com/photo-1499793983690-e29da59ef1c2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2glMjBob3VzZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+            :(v.url==undefined||v.url==null)?v:v.url,
+            
+        
     },
     price: Number,
     location: String,
