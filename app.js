d8c25bf41627216242233b0cda0a85691d409f97
Rishi-Raj-Anand <rishirajanand309@gmail.com>
2025-11-25 17:56:40 +0530

    Final

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ if(process.env.NODE_ENV!="production"){
 }
 const express = require('express');
 const mongoose = require("mongoose");
+const DB_URL = process.env.ATLASDB_URL;
 const app = express();
-let port = 8080;
+let port = process.env.PORT_NUMBER;
 const listingRoute=require('./routes/listing.js')
 const reviewRoute=require('./routes/review.js')
 const userRoute=require('./routes/user.js')
 const cookieParser = require('cookie-parser')
+
 const session = require('express-session')
+const MongoStore = require('connect-mongo');    
+
 const flash = require('connect-flash');
 const passport=require('passport');
 const LocalStrategy=require('passport-local');
@@ -17,16 +21,32 @@ const User=require('./models/user.js')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
+
+const store=MongoStore.create({
+    mongoUrl:DB_URL,
+    dbName: 'vagabond',
+    secret:process.env.SECRETCODE,
+    touchAfter:24*3600,
+});
+
+store.on("error",(err)=>{
+    console.log("Error in MONGO SESSION STORE",err)
+})
+
 const sessionOptions={
-  secret: 'vagabondxyz',
+  store,  
+  secret:process.env.SECRETCODE,
   resave: false,
-  saveUninitialized: true,
+  saveUninitialized: true, 
   cookie: {
         //  secure: true,
      expires:Date.now()+7*24*60*60*1000,
      maxAge:7*24*60*60*1000,
      httpOnly:true}
 }
+
+
+
 app.use(session(sessionOptions))
 
 app.use(flash())
@@ -36,7 +56,7 @@ app.use(passport.session());
 
 passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.deserializeUser(User.deserializeUser()); 
 
 // EJS template
 const ejsmate = require('ejs-mate')
@@ -56,10 +76,10 @@ const review = require('./models/review.js');
 app.use(methodOverride("_method"))
 
 // DB Connection
-const DB_URL = "mongodb://127.0.0.1:27017/vagabond";
-
 async function connectDB(dburl) {
-    await mongoose.connect(dburl);
+    await mongoose.connect(dburl,{
+        dbName: 'vagabond'
+    });
 }
 
 connectDB(DB_URL)
